0bc708a207d4e36b55565996610516d2239a7b1e
rps.re <actions@rps.re>
2021-12-11 07:40:13 +0000

    rps.re

--- src/_rpd/__init__.py
+++ src/_rpd/__init__.py
@@ -5,6 +5,6 @@ Privatized Gateway And HTTP Connections To RPD And More.
 """
 
 from .client.core import *
-from .connections.web import *
 from .connections.gate import *
+from .connections.web import *
 from .file import *
