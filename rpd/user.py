97dda8f5f84fc57fbc9c72f949abd8fde4ad34db
rps.re <actions@rps.re>
2021-12-25 09:50:17 +0000

    Automatic linting of the code

--- rpd/user.py
+++ rpd/user.py
@@ -15,4 +15,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the LICENSE file for the specific language governing permissions and
 limitations under the License.
 """
-
