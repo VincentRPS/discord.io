e25c0f693172e103e8c0ad60ac63c881a05e2ae1
Vincent <vincentbusiness55@gmail.com>
2021-12-26 11:29:46 +0800

    feat: get_gateway_bot
    also changes license header

--- rpd/internal/http.py
+++ rpd/internal/http.py
@@ -1,20 +1,18 @@
-"""
-Apache-2.0
-
-Copyright 2021 VincentRPS
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the LICENSE file for the specific language governing permissions and
-limitations under the License.
-"""
+# Apache-2.0
+#
+# Copyright 2021 VincentRPS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the LICENSE file for the specific language governing permissions and
+# limitations under the License.
 from __future__ import annotations
 
 import asyncio
@@ -70,6 +68,7 @@ class Route:
         self.channel_id = parameters.get("channel_id")
         self.guild_id = parameters.get("guild_id")
 
+    # Ratelimit bucket, to be implemented soon
     @property
     def bucket(self) -> str:
         return f"{self.channel_id}:{self.guild_id}:{self.path}"
@@ -285,6 +284,11 @@ class HTTPClient:
         if self.__session:
             await self.__session.close()
 
+    # Gets the current bot from the gateway
+    def get_gateway_bot(self):
+        return self.request(Route("GET", "/gateway/bot"))
+
+    # Statically logs-in to discord.
     async def _client_login(self, token: str) -> User:
         self.__session = aiohttp.ClientSession(
             connector=self.connector, ws_response_class=DiscordClientWebSocketResponse
@@ -302,5 +306,5 @@ class HTTPClient:
 
         return data
 
-    def _client_logout(self) -> Response[None]:
+    def _client_logout(self):
         return self.request(Route("POST", "/auth/logout"))
