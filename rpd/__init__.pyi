5314401eac31aeb50e7ab2cf5a9d79d038eddc41
pep8 <actions@rps.re>
2021-12-31 17:51:59 +0000

    ci: Automatic linting of the code

--- rpd/__init__.pyi
+++ rpd/__init__.pyi
@@ -1,3 +1,5 @@
+import typing
+
 from rpd.abc import *
 from rpd.boot_text import *
 from rpd.data import *
@@ -5,13 +7,12 @@ from rpd.factories import *
 from rpd.helpers import *
 from rpd.internal.rest import *
 from rpd.internal.websockets import *
-import typing
 
 class VersionInfo(typing.NamedTuple):
     major: int
     minor: int
     micro: int
-    releaselevel: typing.Literal['alpha', 'beta', 'candidate', 'final']
+    releaselevel: typing.Literal["alpha", "beta", "candidate", "final"]
     serial: int
 
 version_info: VersionInfo
