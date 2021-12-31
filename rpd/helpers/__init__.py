249056dcbd068ae01e1ead0d531aaf1cdddd91b0
Vincent <vincentbusiness55@gmail.com>
2022-01-01 02:05:41 +0800

    adding a ciso rpd helper

--- rpd/helpers/__init__.py
+++ rpd/helpers/__init__.py
@@ -5,3 +5,4 @@ Helper utils for RPD.
 """
 from .missing import *
 from .whichjson import *
+from .ciso_parse import *
---
+++ rpd/helpers/ciso_parse.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# cython: language_level=3
+# Copyright (c) 2021-present VincentRPS
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE
+
+try:
+    import ciso8601
+except(ModuleNotFoundError, ImportError):
+    import datetime
+    HAS_CISO = False
+else:
+    HAS_CISO = True
+
+if HAS_CISO:
+    def parse_time(time: str):
+        ciso8601.parse_datetime(time)
+else:
+    def parse_time(time: str):
+        datetime.strptime(time, '%d/%m/%y %H:%M:%S')
