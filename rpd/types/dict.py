e26ce91355a443a671fead5eb6bec765d44053fa
Vincent <vincentbusiness55@gmail.com>
2022-02-02 11:07:20 +0800

    docs: document `Dict`

--- rpd/types/dict.py
+++ rpd/types/dict.py
@@ -27,5 +27,13 @@ from typing import Type
 
 
 def Dict() -> Type[BaseDict[str, Any]]:
-    """Represents the widely used Dict format which RPD uses."""
+    """Represents the widely used Dict format which RPD uses.
+    dict() -> new empty dictionary dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+    d = {} for k, v in iterable:
+        d[k] = v
+dict(**kwargs) -> new dictionary initialized with the name=value pairs
+    in the keyword argument list. For example: dict(one=1, two=2)
+    """
     return BaseDict[str, Any]
