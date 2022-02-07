6005ec8273d2bb8cdbe94b74825e05f151049663
Vincent <vincentbusiness55@gmail.com>
2022-02-07 21:55:13 +0800

    chore: update assets.py description

--- discord/assets.py
+++ discord/assets.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
-"""Represents a partial Discord Emoji.
+"""Represents a Discord asset, i.e a Emoji or Sticker.
 
 ref: https://discord.dev/resources/emoji
 """
