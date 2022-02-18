138fa8b4c2a71e9fdf5c7d6236dee7c651e35e0a
Vincent <vincentbusiness55@gmail.com>
2022-02-18 17:17:20 +0800

    chore: raise when the api version isn't in commission

--- discord/api/rest.py
+++ discord/api/rest.py
@@ -130,6 +130,9 @@ class RESTClient:
         self._session: aiohttp.ClientSession = utils.MISSING
         self.url = f'https://discord.com/api/v{version}'
 
+        if version not in (8, 9, 10):
+            raise DeprecationWarning('The API Version you are running has been decommissioned, please bump the version.')
+
     async def enter(self):
         self._session = aiohttp.ClientSession()
 
