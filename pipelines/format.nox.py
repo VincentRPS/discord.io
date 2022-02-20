a2e03e30aef83f5f70b19024f077d6fe3f6bd965
Vincent <vincentbusiness55@gmail.com>
2022-02-20 10:17:35 +0800

    chore: remove pipelines

--- pipelines/format.nox.py
+++
@@ -1,8 +0,0 @@
-import nox
-
-
-@nox.session()
-def format(session: nox.Session) -> None:
-    session.install('-r', 'dev-requirements.txt')
-    session.run('black', '.', '--check')
-    session.run('isort', '.', '--check')
